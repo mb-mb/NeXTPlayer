89e4ea0505c10d8161d2cf6853ad78ed0dcedcd4
marcelo bianchi <marc.bianchi@gmail.com>
2023-10-02 21:28:35 -0300

    add sessionamanager to get access token

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -10,91 +10,128 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, SPTSessionManagerDelegate, SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate  {
     
+    var window: UIWindow?
+    var aaccesstoken = "BQA9TZcYQFvU7d_TgNPmOiAX5stxHLYdIav2E2kPGJPyhY7pk7Qu8IkiAwuFxSNXL0YdHRFZuUecbjiRDeVM_XqjjetUmGOBBWR0DVeH1xfYQWWoiOEeKAUhoqDpd9MhQur5eOriTFr9yWatawy6MNLMAwa-07Ykm0mC6MwgTDJQCSub4vTfNOKPPEfRA0L7Nm73j4Q"
     
+    let SpotifyClientID = "\(spotifyClientId)"
+    let SpotifyRedirectURL = URL(string: "spotify-ios-quick-start://spotify-login-callback")!
     
-    var accessToken: String?
+    lazy var configuration = SPTConfiguration(
+        clientID: SpotifyClientID,
+        redirectURL: SpotifyRedirectURL
+    )
     
-    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
-      print("connected")
-    }
-    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
-      print("disconnected")
-    }
-    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
-      print("failed")
-    }
-    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
-      print("player state changed")
-    }
-    
-    
-    lazy var appRemote: SPTAppRemote = {
-      let appRemote = SPTAppRemote(configuration: self.configuration, logLevel: .debug)
-      appRemote.connectionParameters.accessToken = self.accessToken
-      appRemote.delegate = self
-      return appRemote
+    lazy var sessionManager: SPTSessionManager = {
+        if let tokenSwapURL = URL(string: "https://[your token swap app domain here]/api/token"),
+           let tokenRefreshURL = URL(string: "https://[your token swap app domain here]/api/refresh_token") {
+            self.configuration.tokenSwapURL = tokenSwapURL
+            self.configuration.tokenRefreshURL = tokenRefreshURL
+            self.configuration.playURI = ""
+        }
+        let manager = SPTSessionManager(configuration: self.configuration, delegate: self)
+        return manager
     }()
     
-    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
-        print("success", session)
-    }
-
-    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
-        print("fail", error)
-    }
-
-    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
-        print("renewed", session)
-    }
-
-    var window: UIWindow?
-    lazy var configuration = SPTConfiguration(clientID: spotifyClientId,
-                                              redirectURL: redirectURL)
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        if AuthManager.shared.isSignedIn {
-            window.rootViewController = TabBarViewController()
-        } else {
-            let navVC = UINavigationController(rootViewController: WelcomeViewController())
-            navVC.navigationBar.prefersLargeTitles = true
-            navVC.viewControllers.first?.navigationItem.largeTitleDisplayMode = .always
-            window.rootViewController = navVC
-        }
-       
-        window.makeKeyAndVisible()
-        self.window = window
-        print(AuthManager.shared.signInURL?.absoluteString)
+    lazy var appRemote: SPTAppRemote = {
+        let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.delegate = self
+        return appRemote
+    }()
     
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let requestedScopes: SPTScope = [.appRemoteControl]
+        self.sessionManager.initiateSession(with: requestedScopes, options: .default)
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
+    
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        let parameters = appRemote.authorizationParameters(from: url);
-
-              if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
-                  appRemote.connectionParameters.accessToken = access_token
-                  self.accessToken = access_token
-              } else if let error_description = parameters?[SPTAppRemoteErrorDescriptionKey] {
-                  // Show the error
-              }
+        self.sessionManager.application(app, open: url, options: options)
+        
         return true
     }
-
+    
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        self.appRemote.connectionParameters.accessToken = session.accessToken
+        self.appRemote.connect()
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        print(error)
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+        print(session)
+    }
+    
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        print("connected")
+        
+        self.appRemote.playerAPI?.delegate = self
+        self.appRemote.playerAPI?.subscribe(toPlayerState: { (result, error) in
+            if let error = error {
+                debugPrint(error.localizedDescription)
+            }
+        })
+        
+        // Want to play a new track?
+        // self.appRemote.playerAPI?.play("spotify:track:13WO20hoD72L0J13WTQWlT", callback: { (result, error) in
+        //     if let error = error {
+        //         print(error.localizedDescription)
+        //     }
+        // })
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+        print("disconnected")
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        print("failed")
+    }
+    
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+        print("player state changed")
+        print("isPaused", playerState.isPaused)
+        print("track.uri", playerState.track.uri)
+        print("track.name", playerState.track.name)
+        print("track.imageIdentifier", playerState.track.imageIdentifier)
+        print("track.artist.name", playerState.track.artist.name)
+        print("track.album.name", playerState.track.album.name)
+        print("track.isSaved", playerState.track.isSaved)
+        print("playbackSpeed", playerState.playbackSpeed)
+        print("playbackOptions.isShuffling", playerState.playbackOptions.isShuffling)
+        print("playbackOptions.repeatMode", playerState.playbackOptions.repeatMode.hashValue)
+        print("playbackPosition", playerState.playbackPosition)
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        if self.appRemote.isConnected {
+            self.appRemote.disconnect()
+        }
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        if let _ = self.appRemote.connectionParameters.accessToken {
+            self.appRemote.connect()
+        }
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
 }
 
